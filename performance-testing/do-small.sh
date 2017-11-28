bacdbd153a48339a5a8b24d1074e5052babb35b4
Kevin Dice <kevin.dice1@gmail.com>
2017-11-28 13:26:56 -0600

    Set up performance testing scripts

--- performance-testing/do-small.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../bin/00-serial-naive small/energy_graphene_toluene.dx small/diffuse_graphene_toluene.dx small/energy_toluene-toluene.dat small/init.dat 4e-5 0.59616124 100000 0 pdb 100 small_sim_graph-tol
---
+++ performance-testing/perf-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+usage() {
+cat <<EOF
+Usage: ./perf-test.sh <example> <bin>
+
+    example: should be the directory containing the example files (big/small)
+    bin: the binary name - assume relative to the bin directory
+
+EOF
+}
+
+if [ $# -ne 2 ]; then
+	usage
+	exit
+elif [ ! -f ../bin/$2 ]; then
+	echo "Error: Binary file to be tested does not exist"
+	usage
+	exit
+elif [ ! -d $1 ]; then
+	echo "Error: Example directory does not exist"
+	usage
+	exit
+fi
+
+
+
+$1/do-example.sh "$parent_path/../bin/$2"

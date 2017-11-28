f096f6f3fc017d6af01d7c49f6b5551087b78e89
Kevin Dice <kevin.dice1@gmail.com>
2017-11-28 13:32:57 -0600

    Add tiny example that only takes 5 seconds on naive serial

---
+++ performance-testing/tiny/do-example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+if [ $# -ne 1 ]; then
+    echo "You must pass a binary to be tested."
+    exit
+elif [ ! -x $1 ]; then
+    echo "Binary provided does not exist"
+    exit
+fi
+
+$1 energy_graphene_toluene.dx diffuse_graphene_toluene.dx energy_toluene-toluene.dat init_coords.dat 4e-5 0.59616124 10000 0 pdb 100 small_sim_graph-tol

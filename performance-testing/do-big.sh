2d1f1795c53cf7f88f7ffa9e6f0d84c58686454b
Kevin Dice <kevin.dice1@gmail.com>
2017-11-27 23:48:56 -0600

    Reorganize example scripts

---
+++ performance-testing/do-big.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../bin/00-serial-naive big/big_energy_graphene_toluene.dx big/big_diffuse_graphene_toluene.dx big/energy_toluene-toluene.dat big/big_init_coords.dat 4e-5 0.59616124 10 0 pdb 1 sim_big_graph-tol

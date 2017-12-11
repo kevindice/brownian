6f06a0e329cbc0f53d049791afb73fb357dd945a
Kevin Dice <kevin.dice1@gmail.com>
2017-12-10 19:22:42 -0600

    Add halmd dependency to install script

--- gsl-install.sh
+++ gsl-install.sh
@@ -1,3 +1,5 @@
+# Get GSL
+
 cd ~
 wget https://mirrors.ocf.berkeley.edu/gnu/gsl/gsl-2.4.tar.gz
 tar -zxvf gsl-*.*.tar.gz
@@ -15,3 +17,29 @@ else
     source ~/.zshrc
 fi
 
+
+# Now get halmd
+cd ~
+TAR="halmd-0.2.1.tar.bz2" URL="http://code.halmd.org/tar"; \
+wget "$URL/$TAR" && openssl dgst -sha512 \
+  -verify <(wget -qO- "$URL/cert.pem" | openssl x509 -noout -pubkey) \
+  -signature <(wget -qO- "$URL/$TAR.sig") "$TAR"
+
+mkdir -p /tmp/halmd_prerequisites$USER && cd /tmp/halmd-prerequisites$USER
+nice make -f ~/halmd/examples/packages.mk -j4 install
+make -f ~/halmd/examples/packages.mk env >> ~/.bashrc
+make -f ~/halmd/examples/packages.mk env >> ~/.zshrc
+
+mkdir -p /tmp/halmd-build$USER && cd /tmp/halmd-build$USER
+cmake ~/halmd -DCMAKE_INSTALL_DIRECTORY=~/opt/halmd
+nice make -j4
+
+~/halmd/halmd --version
+echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.bashrc
+echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.zshrc
+
+if [ ! -f ~/.zshrc ]; then
+    source ~/.bashrc
+else
+    source ~/.zshrc
+fi

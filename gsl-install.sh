d57a324d3b2fda1885ea18a25ffdfc6a9f1780a9
Kevin Dice <kevin.dice1@gmail.com>
2017-12-10 21:21:30 -0600

    Remove helmd

--- gsl-install.sh
+++ gsl-install.sh
@@ -19,34 +19,3 @@ if [ ! -f ~/.zshrc ]; then
 else
     source ~/.zshrc
 fi
-
-
-# Now get halmd
-cd /tmp/setup
-TAR="halmd-0.2.1.tar.bz2" URL="http://code.halmd.org/tar"; \
-wget "$URL/$TAR" && openssl dgst -sha512 \
-  -verify <(wget -qO- "$URL/cert.pem" | openssl x509 -noout -pubkey) \
-  -signature <(wget -qO- "$URL/$TAR.sig") "$TAR"
-
-tar -xvjf halmd-*.tar.bz2
-
-mkdir -p /tmp/halmd_prerequisites$USER
-cd /tmp/halmd_prerequisites$USER
-
-nice make -f /tmp/setup/halmd-0.2.1/examples/packages.mk -j6 install
-make -f /tmp/setup/halmd-0.2.1/examples/packages.mk env >> ~/.bashrc
-make -f /tmp/setup/halmd-0.2.1/examples/packages.mk env >> ~/.zshrc
-
-mkdir -p /tmp/halmd-build$USER && cd /tmp/halmd-build$USER
-cmake /tmp/setup/halmd-0.2.1 -DCMAKE_INSTALL_DIRECTORY=~/opt/halmd
-nice make -j6
-
-/tmp/setup/halmd-0.2.1/halmd --version
-echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.bashrc
-echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.zshrc
-
-if [ ! -f ~/.zshrc ]; then
-    source ~/.bashrc
-else
-    source ~/.zshrc
-fi

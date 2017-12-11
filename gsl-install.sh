a699607d1bc43821d7ffbc046ffc3b5bfe753781
Kevin Dice <kevin.dice1@gmail.com>
2017-12-10 19:24:33 -0600

    Increase concurrency on builds

--- gsl-install.sh
+++ gsl-install.sh
@@ -6,7 +6,7 @@ tar -zxvf gsl-*.*.tar.gz
 mkdir gsl
 cd gsl-2.4
 ./configure --prefix=$HOME/gsl
-make
+make -j6
 make check
 make install
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$HOME/gsl/lib/" >> ~/.bashrc
@@ -26,13 +26,13 @@ wget "$URL/$TAR" && openssl dgst -sha512 \
   -signature <(wget -qO- "$URL/$TAR.sig") "$TAR"
 
 mkdir -p /tmp/halmd_prerequisites$USER && cd /tmp/halmd-prerequisites$USER
-nice make -f ~/halmd/examples/packages.mk -j4 install
+nice make -f ~/halmd/examples/packages.mk -j6 install
 make -f ~/halmd/examples/packages.mk env >> ~/.bashrc
 make -f ~/halmd/examples/packages.mk env >> ~/.zshrc
 
 mkdir -p /tmp/halmd-build$USER && cd /tmp/halmd-build$USER
 cmake ~/halmd -DCMAKE_INSTALL_DIRECTORY=~/opt/halmd
-nice make -j4
+nice make -j6
 
 ~/halmd/halmd --version
 echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.bashrc

cbe4c25391f44ae4cf7f966e4fba04277a369e8e
Kevin Dice <kevin.dice1@gmail.com>
2017-12-10 20:33:34 -0600

    Use local file system

--- gsl-install.sh
+++ gsl-install.sh
@@ -1,9 +1,12 @@
 # Get GSL
 
-cd ~
+cd /tmp
+mkdir setup
+cd setup
+
 wget https://mirrors.ocf.berkeley.edu/gnu/gsl/gsl-2.4.tar.gz
 tar -xvzf gsl-*.*.tar.gz
-mkdir gsl
+mkdir ~/gsl
 cd gsl-2.4
 ./configure --prefix=$HOME/gsl
 make -j6
@@ -19,26 +22,26 @@ fi
 
 
 # Now get halmd
-cd ~
+cd /tmp/setup
 TAR="halmd-0.2.1.tar.bz2" URL="http://code.halmd.org/tar"; \
 wget "$URL/$TAR" && openssl dgst -sha512 \
   -verify <(wget -qO- "$URL/cert.pem" | openssl x509 -noout -pubkey) \
   -signature <(wget -qO- "$URL/$TAR.sig") "$TAR"
 
-tar -xvjf halmd-*.tar.bz2 -C halmd
+tar -xvjf halmd-*.tar.bz2
 
 mkdir -p /tmp/halmd_prerequisites$USER
 cd /tmp/halmd_prerequisites$USER
 
-nice make -f ~/halmd/examples/packages.mk -j6 install
-make -f ~/halmd/examples/packages.mk env >> ~/.bashrc
-make -f ~/halmd/examples/packages.mk env >> ~/.zshrc
+nice make -f /tmp/setup/halmd-0.2.1/examples/packages.mk -j6 install
+make -f /tmp/setup/halmd-0.2.1/examples/packages.mk env >> ~/.bashrc
+make -f /tmp/setup/halmd-0.2.1/examples/packages.mk env >> ~/.zshrc
 
 mkdir -p /tmp/halmd-build$USER && cd /tmp/halmd-build$USER
-cmake ~/halmd -DCMAKE_INSTALL_DIRECTORY=~/opt/halmd
+cmake /tmp/setup/halmd-0.2.1 -DCMAKE_INSTALL_DIRECTORY=~/opt/halmd
 nice make -j6
 
-~/halmd/halmd --version
+/tmp/setup/halmd-0.2.1/halmd --version
 echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.bashrc
 echo "export PATH=\"${HOME}/opt/halmd/bin\${PATH+:\$PATH}\"" >> ~/.zshrc
 

cd34cebeae3b4b690ad0110740cfe7206b39cceb
George W. Walker <george@georgewwalker.com>
2017-12-16 22:31:30 -0600

    Fixed?

--- exampleFindNeighborCells.c
+++ exampleFindNeighborCells.c
@@ -5,55 +5,44 @@
 // how the cell index is computed
 // thisCellIndex = ((pos[i].x + (sizeX/2))/numCellsX) * numCellsY * numCellsZ + ((pos[i].y + (sizeY/2))/numCellsY) * numCellsZ + ((pos[i].z + (sizeZ/2))/numCellsZ);
 
+
+int scalarIndexCompose(int x, int y, int z, int dx, int dy, int dz, int numCellsX, int numCellsY, int numCellsZ) {
+    return ((dx + x) mod numCellsX) + ((dy + y) mod numCellsY) * numCellsX + ((dz + z) mod numCellsZ) * numCellsX * numCellsY;
+}
+
+int[] scalarIndexDecompose(int numCellsX, int numCellsY, int numCellsZ, int thisCellIndex) {
+    int xyz[3], x, y, z;
+
+    x = thisCellIndex mod numCellsX;
+    y = (((thisCellIndex - x)) / numCellsX) mod numCellsY;
+    z = (thisCellIndex - y * numCellsX - x) / (numCellsX * numCellsY);
+
+    xyz[0] = x;
+    xyz[1] = y;
+    xyz[3] = z;
+    return xyz;
+}
+
+
 int[] findNeighborCells(int thisCellIndex, int numCellsX, int numCellsY, int numCellsZ) {
     int neighborIndexes[27];
-    int numCells = numCellsX * numCellsY * numCellsZ;
 
-    // This cell
-    neighborIndexes[0] = thisCellIndex;
+    int neighborIndexesIndex = 0;
 
-    // X-direction
-    neighborIndexes[1] = (thisCellIndex + numCells - 1) % numCells; // cell to left -- need to check
-    neighborIndexes[2] = (thisCellIndex + numCells + 1) % numCells; // cell to right -- not right (should it be numCellsX in the middle?)
+    int xyz[3];
 
-    // Y-direction
-    neighborIndexes[3] = (thisCellIndex + numCells + (-1 * numCellsZ)) % numCells; // cell above -- need to check
-    neighborIndexes[4] = (thisCellIndex + numCells + (1 * numCellsZ)) % numCells; // cell below -- need to check
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            for (int k = -1; k <= 1; k++) {
 
-    // Z-direction
-    neighborIndexes[5] = (thisCellIndex + numCells + (-1 * numCellsY * numCellsZ)) % numCells; // cell behind -- need to check
-    neighborIndexes[6] = (thisCellIndex + numCells + (1 * numCellsY * numCellsZ)) % numCells; // cell in front -- need to check
+                xyz = scalarIndexDecompose(numCellsX, numCellsY, numCellsZ, thisCellIndex);
 
+                neighborIndexes[neighborIndexesIndex] = scalarIndexCompose(xyz[0], xyz[1], xyz[2], i, j, k, numCellsX, numCellsY, numCellsZ);
+                neighborIndexesIndex++;
+            }
+        }
 
-// NOT YET IMPLEMENTED/CORRECT:
-    // X-Y
-    neighborIndexes[7] = (thisCellIndex + numCells - 1 + (-1 * numCellsZ)) % numCells; // cell above and left
-    neighborIndexes[8] = (thisCellIndex + numCells + 1 + (-1 * numCellsZ)) % numCells; // cell above and right
-    neighborIndexes[9] = (thisCellIndex + numCells - 1 + (1 * numCellsZ)) % numCells; // cell below and left
-    neighborIndexes[10] = (thisCellIndex + numCells + 1 + (1 * numCellsZ)) % numCells; // cell below and right
-
-    // X-Z
-    neighborIndexes[11] = ; // cell behind and left
-    neighborIndexes[12] = ; // cell behind and right
-    neighborIndexes[13] = ; // cell in front and left
-    neighborIndexes[14] = ; // cell in front and right
-
-    // Y-Z
-    neighborIndexes[15] = ; // cell above and behind
-    neighborIndexes[16] = ; // cell above and in front
-    neighborIndexes[17] = ; // cell below and behind
-    neighborIndexes[18] = ; // cell below and in front
-
-    // X-Y-Z
-    neighborIndexes[19] = ; // cell above, left, behind
-    neighborIndexes[20] = ; // cell above, right, behind
-    neighborIndexes[21] = ; // cell above, left, in front
-    neighborIndexes[22] = ; // cell above, right, in front
-
-    neighborIndexes[23] = ; // cell below, left, behind
-    neighborIndexes[24] = ; // cell below, right, behind
-    neighborIndexes[25] = ; // cell below, left, in front
-    neighborIndexes[26] = ; // cell below, right, in front
+    }
 
     return neighborIndexes;
 }

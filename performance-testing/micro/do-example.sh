249a71b4acff0e5e1a3ccbb1979e9c9d2f077e44
Kevin Dice <kevin.dice1@gmail.com>
2018-05-31 20:33:30 -0500

    Add micro example

---
+++ performance-testing/micro/do-example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+if [ $# -ne 1 ]; then
+    echo "You must pass a binary to be tested."
+    exit
+elif [ ! -x $1 ]; then
+    echo "Binary provided does not exist"
+    exit
+fi
+
+$1 energy_graphene_toluene.dx diffuse_graphene_toluene.dx energy_toluene-toluene.dat init_coords.dat 4e-5 0.59616124 10 0 pdb 1 micro_sim_graph-tol-$(basename "$1")-$SGE_JOB_ID-$SGE_TASK_ID-$(date +%s)
